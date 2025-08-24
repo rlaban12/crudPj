1c0af5077f329886aae7ab1032b499f8ba4fa0de
rlaban12 <rlaeogus9786@gmail.com>
2025-08-24 15:33:39 +0900

    second commit

---
+++ src/main/java/com/spring/crudpj/CrudPjApplication.java
@@ -0,0 +1,13 @@
+package com.spring.crudpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudPjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/crudpj/CrudPjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.crudpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
