4295b85fcbd69b5aa9733f41815c81d7bf8edbae
rlaban12 <rlaeogus9786@gmail.com>
2025-09-03 17:48:00 +0900

    백엔드 api 확인, 프론트엔드 메인화면, 로그인 모달 창 추가

---
+++ src/main/resources/static/js/config/routes-config.js
@@ -0,0 +1,20 @@
+// 페이지 라우팅 설정
+/*
+  module: 해당 경로에 들어오면 실행할 모듈 js
+  requiresAuth: 인증된 사용자만 들어오는 페이지인지 여부
+ */
+
+export const PAGE_CONFIG = {
+    '/': {
+        module: 'home',
+        requiresAuth: false,
+    },
+    '/login': {
+        module: 'login',
+        requiresAuth: false,
+    },
+    '/signup': {
+        module: 'signup',
+        requiresAuth: false,
+    },
+};
